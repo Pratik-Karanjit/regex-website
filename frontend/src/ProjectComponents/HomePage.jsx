8f964c0f1d01023aa26a98f8c0e250da8e4982e9
Pratik Karanjit <pratikkaranjit@gmail.com>
2023-09-18 18:14:00 +0545

    logo

--- frontend/src/ProjectComponents/HomePage.jsx
+++ frontend/src/ProjectComponents/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Tabs from './Tabs';
 import Axios from 'axios';
+import logo from '../ProjectCSS/Regex website logo.png'
 
 const HomePage = () => {
   const [searchQuery, setSearchQuery] = useState('');
@@ -37,45 +38,59 @@ const HomePage = () => {
   };
 
   return (
-    <div className="container mt-5">
-      <h1 className="text-center">Search Regex Patterns</h1>
-      <div className="input-group mb-3">
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Search by requirements..."
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <button className="btn btn-primary" onClick={handleSearch}>
-          Search
-        </button>
+    <div className="container mt-3"> {/* Updated margin-top to mt-3 */}
+      <div className="row">
+        <div className="col-12 text-center">
+          <img src={logo} alt="Image Description" style={{ maxWidth: '200px' }} />
+        </div>
       </div>
-
-      <div>
-        <h2>Matching Patterns</h2>
-        {filteredPatterns && filteredPatterns.length > 0 ? (
-          <ul className="list-group">
-            {filteredPatterns.map((pattern) => (
-              <li key={pattern.id} className="list-group-item">
-                <strong>Name:</strong> {pattern.name}
-                <br />
-                <strong>Requirements:</strong> {pattern.requirements}
-                <br />
-                <strong>Pattern:</strong> {pattern.pattern}
-                <br />
-                <button
-                  className="btn btn-primary"
-                  onClick={() => handleCopyPattern(pattern.pattern)}
-                >
-                  Copy Pattern
-                </button>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>No matching patterns found.</p>
-        )}
+      <div className="row">
+        <div className="col-12 text-center">
+          <h1>Search Regex Patterns</h1>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search by requirements..."
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button className="btn btn-primary" onClick={handleSearch}>
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <h2 className="text-center">Matching Patterns</h2>
+          {filteredPatterns && filteredPatterns.length > 0 ? (
+            <ul className="list-group">
+              {filteredPatterns.map((pattern) => (
+                <li key={pattern.id} className="list-group-item">
+                  <strong>Name:</strong> {pattern.name}
+                  <br />
+                  <strong>Requirements:</strong> {pattern.requirements}
+                  <br />
+                  <strong>Pattern:</strong> {pattern.pattern}
+                  <br />
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => handleCopyPattern(pattern.pattern)}
+                  >
+                    Copy Pattern
+                  </button>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p className="text-center">No matching patterns found.</p>
+          )}
+        </div>
       </div>
       <Tabs />
     </div>
